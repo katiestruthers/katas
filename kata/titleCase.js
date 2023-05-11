a85e7a871687522e6c2d9ea3af91cfb83d2a9e58
Katherine Struthers <ktstruthers@gmail.com>
2023-05-10 20:57:36 -0500

    fix function to pass all tests

--- kata/titleCase.js
+++ kata/titleCase.js
@@ -1,5 +1,22 @@
-const titleCase = function(string) {
+const titleCase = function(text) {
+  let capitalized = '';
+  let previousIsSpace = true;
 
+  for (const letter of text.toLowerCase()) {
+    if (previousIsSpace) {
+      capitalized += letter.toUpperCase();
+      previousIsSpace = false;
+      continue;
+    }
+
+    if (letter === ' ') {
+      previousIsSpace = true;
+    }
+    
+    capitalized += letter;
+  }
+
+  return capitalized;
 };
 
 module.exports = titleCase;
